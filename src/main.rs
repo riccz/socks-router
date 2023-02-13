c2a54db6a54fc6a26085724c016e93c2354e5113
Riccardo Zanol <rz@riccza.nl>
2023-02-13 17:35:42 +0100

    Better logging for the watcher task

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use anyhow::{Context, Result};
 use notify::RecursiveMode;
 use tokio::sync::mpsc;
 use tokio::task::JoinHandle;
-use tracing::{debug, info};
+use tracing::{debug, debug_span, info, info_span, trace, Instrument};
 use tracing_subscriber::FmtSubscriber;
 
 mod client;
@@ -68,7 +68,9 @@ async fn inner_main() -> Result<()> {
     let authenticator = SimpleAuthenticator {};
     let mut server = Server::new(&STATIC_CONF.read().listen, router.clone(), authenticator).await?;
 
-    let (watcher_handle, mut watcher_rx) = run_watcher(dynconf_path)?;
+    // Setup the watcher task
+    let watcher_span = debug_span!("watcher", path = ?dynconf_path);
+    let (watcher_handle, mut watcher_rx) = watcher_span.in_scope(|| run_watcher(dynconf_path))?;
 
     loop {
         tokio::select! {
@@ -78,7 +80,7 @@ async fn inner_main() -> Result<()> {
                 break
             }
             res = watcher_rx.recv() => {
-                res.expect("Dropped the sender"); // This happens when the watcher task errors
+                let _events = res.expect("The mpsc sender was dropped")?;
 
                 // Reread the config
                 let new_config = DynConf::load()?;
@@ -96,10 +98,14 @@ async fn inner_main() -> Result<()> {
     }
 
     // Shutdown the watcher
-    watcher_handle.abort();
-    if let Err(e) = watcher_handle.await {
-        debug!("Aborted the watcher with {}", e);
+    async {
+        watcher_handle.abort();
+        if let Err(e) = watcher_handle.await {
+            debug!(%e, "Stopped the watcher");
+        }
     }
+    .instrument(watcher_span)
+    .await;
 
     Ok(())
 }
@@ -109,30 +115,57 @@ async fn inner_main() -> Result<()> {
 /// When the file changes, the `mpsc::Receiver` returned by this function gets a
 /// new message.  The watcher runs forever, unless the task is cancelled via the
 /// returned `JoinHandle`.
+///
+/// The errors from the AsyncWatcher are passed into the channel and should be handled at the receiver side.
+/// They can't accumulate with automatic retries, because the channel has capacity 1.
 fn run_watcher<P: AsRef<Path>>(
     watch_path: P,
-) -> Result<(JoinHandle<Result<()>>, mpsc::Receiver<()>)> {
+) -> Result<(
+    JoinHandle<Result<()>>,
+    mpsc::Receiver<Result<Vec<notify::Event>, notify::Error>>,
+)> {
     // Watch the parent directory and filter on the path, because some editors
     // (e.g. vim) rename the original to backup and replace with a new file.
     // FIXME: check the event type  and unwatch/watch the new file
     let watch_path = watch_path.as_ref().canonicalize()?;
     let parent_dir = watch_path.parent().unwrap();
+    debug!(?parent_dir, "Watching parent dir");
 
     let mut watcher = AsyncWatcher::new()?;
     watcher.watch(parent_dir, RecursiveMode::Recursive)?;
 
+    // Capacity 1 is important for errors: avoid retrying in the loop and accumulating more than one error.
     let (tx, rx) = mpsc::channel(1);
-    let handle = tokio::spawn(async move {
-        loop {
-            let events = watcher
-                .next_debounced_filter(Duration::from_millis(500), |e| {
-                    e.paths.contains(&watch_path)
-                })
-                .await?;
-            if events.len() > 0 {
-                tx.send(()).await.expect("Dropped the receiver");
+    let handle = tokio::spawn(
+        async move {
+            debug!("Starting watcher task");
+            // This never exits: errors are passed into the mpsc channel
+            // The only wait to stop this is to abort the task via the JoinHandle
+            loop {
+                let res_events = watcher
+                    .next_debounced_filter(Duration::from_millis(500), |e| {
+                        e.paths.contains(&watch_path)
+                    })
+                    .await;
+                match res_events {
+                    Ok(events) => {
+                        let event_count = events.len();
+                        assert!(event_count > 0);
+                        debug!(?event_count, "Watcher saw at least one event");
+                        tx.send(Ok(events))
+                            .await
+                            .expect("The mpsc receiver was dropped");
+                    }
+                    Err(e) => {
+                        trace!(%e, "Error while watching for file changes");
+                        tx.send(Err(e))
+                            .await
+                            .expect("The mpsc receiver was dropped");
+                    }
+                }
             }
         }
-    });
+        .in_current_span(),
+    );
     Ok((handle, rx))
 }
